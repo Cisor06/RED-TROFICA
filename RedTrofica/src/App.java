49c8f470d532d4842086270b6a3ebce2b201248c
jheiber <jheiberb@gmail.com>
2025-10-24 16:06:37 -0500

    primer

--- RedTrofica/src/App.java
+++ RedTrofica/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("hola mundo");
+        System.out.println("hola");
     }
 }
